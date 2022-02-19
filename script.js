7ce7f4b122b0d5c583fd30265a2689f852f35012
Abi <91515654+awhittle2@users.noreply.github.com>
2022-02-19 08:31:00 -0800

    Create script.js
    Attempt to make the game work with java script

---
+++ script.js
@@ -0,0 +1,11 @@
+var playing = false
+let buttonString = document.getElementById("btn")
+
+function startGame() {
+    playing = true;
+    buttonString.textContent = "Click"
+}
+
+while(playing === true) {
+
+}
