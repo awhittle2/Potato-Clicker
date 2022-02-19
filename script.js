7a593b9a73a611317e09c31df5344f6594355faa
Abi <91515654+awhittle2@users.noreply.github.com>
2022-02-19 09:32:35 -0800

    Update script.js
    Nothing yet

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-var playing = false
-let buttonString = document.getElementById("btn")
 
-function startGame() {
-    playing = true;
-    buttonString.textContent = "Click"
-}
-
-while(playing === true) {
-
-}
